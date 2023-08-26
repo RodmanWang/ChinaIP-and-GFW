f1d579dfccc859c6b830ef4618d7014f06efccf0
RodmanWang <19368219+RodmanWang@users.noreply.github.com>
2023-08-26 21:01:03 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,148 +2,141 @@ package main
 
 import (
 	"bufio"
-	"math/big"
-	"net/http"
-	"net/netip"
+	"log"
+	"net"
 	"os"
-	"strconv"
+	"strings"
+
+	"github.com/EvilSuperstars/go-cidrman"
+	"github.com/maxmind/mmdbwriter"
+	"github.com/maxmind/mmdbwriter/mmdbtype"
+	"github.com/spf13/pflag"
 )
 
 func main() {
-	err := test()
+	var (
+		cidrSource []string
+		cidrOutput string
+		mmdbOutput string
+		showHelp   bool
+	)
+
+	pflag.StringSliceVarP(&cidrSource, "source", "i", nil, "CIDR source files")
+	pflag.StringVarP(&cidrOutput, "cidr-output", "o", "cidr.txt", "CIDR ouput")
+	pflag.StringVarP(&mmdbOutput, "mmdb-output", "m", "Country.mmdb", "MMDB ouput")
+	pflag.BoolVarP(&showHelp, "help", "h", false, "Show usage")
+	pflag.CommandLine.SortFlags = false
+	pflag.Parse()
+
+	if showHelp {
+		pflag.Usage()
+		os.Exit(0)
+	}
+
+	if len(cidrSource) < 1 {
+		log.Fatal("at least one cidr source file is required")
+	}
+
+	data, err := merge(cidrSource)
 	if err != nil {
-		panic(err)
+		log.Fatal("merge cidr: " + err.Error())
+	}
+
+	if err := saveFile(data, cidrOutput); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := buildMMDB(data, mmdbOutput); err != nil {
+		log.Fatal(err)
 	}
 }
 
-func test() error {
-	u := "https://github.com/Hackl0us/GeoIP2-CN/raw/release/CN-ip-cidr.txt"
-	req, err := http.NewRequest(http.MethodGet, u, nil)
-	if err != nil {
-		return err
-	}
+func merge(source []string) ([]*net.IPNet, error) {
+	cidrMap := make(map[string]struct{})
 
-	c := &http.Client{
-		// Transport: &http.Transport{
-		// 	Proxy: func(r *http.Request) (*url.URL, error) {
-		// 		return url.Parse("http://127.0.0.1:1080")
-		// 	},
-		// },
-	}
-
-	resp, err := c.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	var (
-		cidr, ipv4Cidr, ipv6Cidr []string
-
-		br      = bufio.NewScanner(resp.Body)
-		rangeIp = []*big.Int{{}, {}, big.NewInt(1)}
-		lastV4  = []*big.Int{{}, {}}
-		lastV6  = []*big.Int{{}, {}}
-		fillBuf = make([]byte, 16)
-	)
-	for br.Scan() {
-		ip, err := netip.ParsePrefix(br.Text())
+	for _, file := range source {
+		f, err := os.Open(file)
 		if err != nil {
+			return nil, err
+		}
+		scanner := bufio.NewScanner(f)
+		scanner.Split(bufio.ScanLines)
+		for scanner.Scan() {
+			line := strings.TrimSpace(scanner.Text())
+			if line == "" {
+				continue
+			}
+			cidrMap[line] = struct{}{}
+		}
+		f.Close()
+	}
+
+	ips := make([]*net.IPNet, 0, len(cidrMap))
+	for cidr := range cidrMap {
+		_, c, err := net.ParseCIDR(cidr)
+		if err != nil {
+			log.Println(err)
 			continue
 		}
-
-		// rangeIp[0] 当前行起始ip地址
-		rangeIp[0].SetBytes(ip.Addr().AsSlice())
-		rangeIp[1].Set(rangeIp[0])
-
-		last, tmp, ipCidr, setBit := lastV4, fillBuf[:4], &ipv4Cidr, 31
-		if ip.Addr().Is6() {
-			last, tmp, ipCidr, setBit = lastV6, fillBuf, &ipv6Cidr, 127
-		}
-
-		for i := setBit - ip.Bits(); i >= 0; i-- {
-			rangeIp[1].SetBit(rangeIp[1], i, 1)
-		}
-		rangeIp[1].Add(rangeIp[1], rangeIp[2])
-
-		if last[1].Cmp(rangeIp[0]) == 0 {
-			// 本行起始ip是上一行结束ip+1,可以组成连续ip范围
-			last[1].Set(rangeIp[1])
-		} else {
-			if last[1].BitLen() > 0 {
-				cidr = ipRangeToCIDR(cidr[:0], tmp, last[0], last[1].Sub(last[1], rangeIp[2]))
-				*ipCidr = append(*ipCidr, cidr...) // 根据ip起止范围计算CIDR表达式
-			}
-			last[0].Set(rangeIp[0])
-			last[1].Set(rangeIp[1])
-		}
+		ips = append(ips, c)
 	}
-	err = br.Err()
-	if err != nil {
-		return err
-	}
-
-	if lastV4[1].BitLen() > 0 {
-		cidr = ipRangeToCIDR(cidr[:0], fillBuf[:4], lastV4[0], lastV4[1].Sub(lastV4[1], rangeIp[2]))
-		ipv4Cidr = append(ipv4Cidr, cidr...)
-	}
-	if lastV6[1].BitLen() > 0 {
-		cidr = ipRangeToCIDR(cidr[:0], fillBuf, lastV6[0], lastV6[1].Sub(lastV6[1], rangeIp[2]))
-		ipv6Cidr = append(ipv6Cidr, cidr...)
-	}
-
-	fwCidr := func(name string, cidr []string) error {
-		fw, err := os.Create(name)
-		if err != nil {
-			return err
-		}
-		defer fw.Close()
-		for _, v := range cidr {
-			fw.WriteString(v + "\n")
-		}
-		return nil
-	}
-
-	err = fwCidr("ipv4.txt", ipv4Cidr)
-	if err != nil {
-		return err
-	}
-	err = fwCidr("ipv6.txt", ipv6Cidr)
-	if err != nil {
-		return err
-	}
-	return nil
+	return cidrman.MergeIPNets(ips)
 }
 
-func ipRangeToCIDR(cidr []string, buf []byte, ipsInt, ipeInt *big.Int) []string {
-	var (
-		tmpInt = new(big.Int)
-		mask   = new(big.Int)
-		one    = big.NewInt(1)
-		bits   uint
-		maxBit = uint(len(buf) * 8)
-	)
-	for {
-		bits = 1
-		mask.SetUint64(1)
-		for bits < maxBit {
-			if (tmpInt.Or(ipsInt, mask).Cmp(ipeInt) > 0) ||
-				(tmpInt.Lsh(tmpInt.Rsh(ipsInt, bits), bits).Cmp(ipsInt) != 0) {
-				bits--
-				mask.Rsh(mask, 1)
-				break
-			}
-			bits++
-			mask.Add(mask.Lsh(mask, 1), one)
-		}
-
-		addr, _ := netip.AddrFromSlice(ipsInt.FillBytes(buf))
-		cidr = append(cidr, addr.String()+"/"+strconv.FormatUint(uint64(maxBit-bits), 10))
-
-		if tmpInt.Or(ipsInt, mask); tmpInt.Cmp(ipeInt) >= 0 {
-			break
-		}
-		ipsInt.Add(tmpInt, one)
+func saveFile(data []*net.IPNet, output string) error {
+	outputF, err := os.Create(output)
+	if err != nil {
+		return err
 	}
-	return cidr
+	defer outputF.Close()
+
+	writer := bufio.NewWriter(outputF)
+	for _, v := range data {
+		writer.WriteString(v.String() + "\n")
+	}
+	return writer.Flush()
+}
+
+func buildMMDB(data []*net.IPNet, output string) error {
+	outputF, err := os.Create(output)
+	if err != nil {
+		return err
+	}
+	defer outputF.Close()
+
+	writer, err := mmdbwriter.New(mmdbwriter.Options{
+		DatabaseType: "GeoIP2-Country",
+		RecordSize:   24,
+	})
+	if err != nil {
+		return err
+	}
+
+	// https://github.com/Hackl0us/GeoIP2-CN/blob/c053afa7ef3d092b1ea84aa229fe035a49fe3603/main.go#L64
+	// https://dev.maxmind.com/geoip/docs/databases/city-and-country
+	// https://dev.maxmind.com/static/pdf/GeoLite2-and-GeoIP2-Precision-Web-Services-Comparison.pdf
+	dataType := mmdbtype.Map{
+		"country": mmdbtype.Map{
+			"geoname_id":           mmdbtype.Uint32(1814991),
+			"is_in_european_union": mmdbtype.Bool(false),
+			"iso_code":             mmdbtype.String("CN"),
+			"names": mmdbtype.Map{
+				"de":    mmdbtype.String("China"),
+				"en":    mmdbtype.String("China"),
+				"es":    mmdbtype.String("China"),
+				"fr":    mmdbtype.String("Chine"),
+				"ja":    mmdbtype.String("中国"),
+				"pt-BR": mmdbtype.String("China"),
+				"ru":    mmdbtype.String("Китай"),
+				"zh-CN": mmdbtype.String("中国"),
+			},
+		},
+	}
+	for _, v := range data {
+		if err := writer.Insert(v, dataType); err != nil {
+			log.Println("fail to insert " + v.String())
+		}
+	}
+	_, err = writer.WriteTo(outputF)
+	return err
 }
